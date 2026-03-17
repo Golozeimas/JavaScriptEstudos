bd7df8a20fd38b31bec1eca70371f31b56e3ebfe
Matheus+ <joaosobramatheus@gmail.com>
2026-03-17 12:17:30 -0300

    study: aprendizado sobre arrays e loops no JS

---
+++ loops/script.js
@@ -0,0 +1,16 @@
+x = 0;
+
+while(x <= 40){
+    document.write("<h2> O valor do X é: " + x + "</h2>");
+    x++;
+}
+
+for(let a = 0; a <= 30; a++){
+    document.write("<h3> O valor do A é: " + a + "</h3>");
+}
+
+const valor = 20;
+
+for(let i = 0; i < valor; i++){
+    document.write("<h4> O valor de i é: " + i + "</h4>")
+}
