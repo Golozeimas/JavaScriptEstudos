ad35ed595bf84c8cb2755d02369f43c9611c3532
Matheus+ <joaosobramatheus@gmail.com>
2026-04-10 12:20:29 -0300

    study: aprendizado sobre fetch e requisições HTTP no JS

---
+++ RequisiçõesHTTP/script.js
@@ -0,0 +1,26 @@
+
+let listElement = document.querySelector("#app");
+
+
+let spells = [];
+
+
+function nutriApp(){
+    fetch('https://potterapi-fedeperin.vercel.app/en/spells')
+    .then((response) => response.json()) // podemos usar assim quando é apenas uma linha, '.json' transforma em json 
+    .then((json) => {
+        spells = json;
+        spells.map((item) => {
+            let liElement = document.createElement("li");
+            let textSpells = document.createTextNode(item.spell);
+            liElement.appendChild(textSpells);
+            listElement.appendChild(liElement);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+}
+
+nutriApp()
